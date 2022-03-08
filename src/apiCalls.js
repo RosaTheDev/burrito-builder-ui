1bb7d107d5036b8dbdc493d4920880b6877a3552
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-03-08 10:47:22 -0700

    Created a post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,19 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const postOrders = (name, ingredients) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: "POST",
+    body: JSON.stringify({
+      name: name,
+      ingredients: ingredients
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(data => console.log('post data', data))
+  .catch(err => console.log(err))
+}
